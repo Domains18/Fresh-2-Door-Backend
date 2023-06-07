386f359b6e6b970d023d9483f57bf9b5d5a7b326
Domains18 <gibsonsgibson88@gmail.com>
2023-06-07 13:34:41 +0300

    removed server
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const api = express();
 const cors = require('cors');   // Cross Origin Resource Sharing
 const colors = require('colors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 // Middlewares
 api.use(cors());
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
 api.use(bodyParser.json());
+api.use
 
-
-
+// Routes
+api.use('/', require('./routers/root.router'));
 
 
 
