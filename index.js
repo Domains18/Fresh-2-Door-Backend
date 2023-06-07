a569fe9c6461ad26de3045dbebec913b95f7e5b8
Domains18 <gibsonsgibson88@gmail.com>
2023-06-07 14:23:45 +0300

    server setup
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ api.use(cors());
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
 api.use(bodyParser.json());
-api.use
+api.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 api.use('/', require('./routers/root.router'));
-
+api.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'public', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: 'Not found' });
+    } else if (req.accepts('text')) {
+        res.send('Not found');
+    }
+    
+});
 
 
 
