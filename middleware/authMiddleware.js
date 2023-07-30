9a098173963206f877d52ff1869c2b255c38ddff
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-07-30 17:36:14 +0300

    authentication middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const expressAsyncHandler = require('express-async-handler');
+const User = require('../models/userschema');
+
+const protect = expressAsyncHandler(async (req, res, next) =>{
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try{
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            console.log(req.user);
+            next();
+        }catch(error){
+            console.error(error);
+            res.status(401);
+            throw new Error('Not authorized, token failed');
+        }
+    }
+    if(!token){
+        res.staus(401);
+        throw new Error('Not authorized, no token');
+    }
+});
+
+
+module.exports = { protect };
