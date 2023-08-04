e74e21238dd9b2d8e6712a046d71c64fd873edab
Gibsons Gibson <gibsonsgibson88@gmail.com>
2023-08-04 05:27:19 +0300

    incomplete commit on order service

--- services/order.service.js
+++ services/order.service.js
@@ -34,3 +34,96 @@ const makeOrder = expressAsyncHandler(async (req, res) => {
         return res.sendStatus(500)
     }
 });
+
+
+const getOrders = expressAsyncHandler(async (req, res) => {
+    try {
+        const orders = await Order.find({ user: req.user._id });
+        if(orders) {
+            res.status(200).json({ message: 'Orders fetched successfully', orders });
+        } else {
+            res.status(400).json({ message: 'Orders not fetched' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+});
+
+const getOrderById = expressAsyncHandler(async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+        if(order) {
+            res.status(200).json({ message: 'Order fetched successfully', order });
+        } else {
+            res.status(400).json({ message: 'Order not fetched' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+});
+
+const updateOrderToPaid = expressAsyncHandler(async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+        if(order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.paymentResult = {
+                id: req.body.id,
+                status: req.body.status,
+                update_time: req.body.update_time,
+                email_address: req.body.email_address,
+            }
+            const updatedOrder = await order.save();
+            res.status(200).json({ message: 'Order paid successfully', order: updatedOrder });
+        } else {
+            res.status(400).json({ message: 'Order not paid' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+});
+
+
+const updateOrderToDelivered = expressAsyncHandler(async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id);
+        if(order) {
+            order.isDelivered = true;
+            order.deliveredAt = Date.now();
+            const updatedOrder = await order.save();
+            res.status(200).json({ message: 'Order delivered successfully', order: updatedOrder });
+        } else {
+            res.status(400).json({ message: 'Order not delivered' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+});
+
+const getOrdersAdmin = expressAsyncHandler(async (req, res) => {
+    try {
+        const orders = await Order.find({}).populate('user', 'name');
+        if(orders) {
+            res.status(200).json({ message: 'Orders fetched successfully', orders });
+        } else {
+            res.status(400).json({ message: 'Orders not fetched' });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(500)
+    }
+});
+
+module.exports = {
+    makeOrder,
+    getOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrdersAdmin,
+}
